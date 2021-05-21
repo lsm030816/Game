5cb71beb6ca1ce8c1c6ff47536e587a3624a704c
lsm030816 <83812965+lsm030816@users.noreply.github.com>
2021-05-21 18:26:43 +0900

    ksw617

---
+++ Assets/PlayerRadar.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRadar : MonoBehaviour
+{
+    [SerializeField] private PlayerController playerController;
+
+    public LayerMask characterMask;
+
+    private void Awake()
+    {
+        playerController = transform.parent.GetComponent<PlayerController>();
+
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("FindTarget", 0.5f, 0.5f);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer).Equals("Character"))
+        {
+            playerController.npcList.Add(other.name);
+
+            string[] types = other.gameObject.name.Split('_');
+
+            switch(types[0])
+            {
+             
+            }
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer).Equals("Character"))
+        {
+            playerController.npcList.Remove(other.name);
+        }
+
+    }
+
+
+}
